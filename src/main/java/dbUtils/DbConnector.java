9defb55834463303c341105063f180e4d887aec8
Bruno <you@example.com>
2022-07-05 17:30:49 -0300

    pase todo lo base de los Dao

---
+++ src/main/java/dbUtils/DbConnector.java
@@ -0,0 +1,62 @@
+package Dao;
+
+import java.sql.*;
+
+public class DbConnector {
+
+	private static DbConnector instancia;
+	
+	private String driver="com.mysql.cj.jdbc.Driver";
+	private String host="localhost";
+	private String port="3306";
+	private String user="java";
+	private String password="himitsu";
+	private String db="java";
+	private int conectados=0;
+	private Connection conn=null;
+	
+	private DbConnector() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static DbConnector getInstancia() {
+		if (instancia == null) {
+			instancia = new DbConnector();
+		}
+		return instancia;
+	}
+	
+	public  void tryRollback() {
+		try { if(conn!=null) { conn.rollback(); } } 
+		catch (SQLException e1) { e1.printStackTrace(); }
+	}
+	
+	public Connection getConn() {
+		try {
+			if(conn==null || conn.isClosed()) {
+				conn=DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db, user, password);
+				conectados=0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conectados++;
+		return conn;
+	}
+	
+	public void releaseConn() {
+		conectados--;
+		try {
+			if (conectados<=0) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
