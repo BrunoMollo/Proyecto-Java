808432fa3378451dea58e4b2c62c681ff76de89e
Ramiro-DG <dia.digiacinti.ramiro@gmail.com>
2022-10-28 18:34:13 -0300

    inicio de metodos de lineaventaDao

--- src/main/java/data/LineaVentaDao.java
+++ src/main/java/data/LineaVentaDao.java
@@ -5,16 +5,25 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 
 import entities.LineaVenta;
+import entities.Venta;
 import ourLib.dbUtils.Dao;
+import ourLib.dbUtils.StatementWrapper;
 
 public class LineaVentaDao extends Dao<LineaVenta>{
 
 	@Override
 	protected LineaVenta mapFromResulset(ResultSet rs) throws SQLException {
-		// TODO Auto-generated method stub
-		String funcName=new Throwable().getStackTrace()[0].getMethodName();
-		throw new UnsupportedOperationException("Not Implemented "+funcName);
+		LineaVenta lv=new LineaVenta();
+		lv.setNroVenta(rs.getInt("nroVenta"));
+		lv.setCodBarra(rs.getInt("codBarra"));
+		lv.setCantidad(rs.getInt("cantidad"));
+		lv.setPrecioUnidad(rs.getDouble("precioUnidad"));
+		return lv;
 	}
+	public LinkedList<LineaVenta> getAllFromVenta(Venta v) throws SQLException {
+		return doFindAll(new StatementWrapper("select * from linea_ventas where nroVenta=?").push(v.getNroVenta()));
+	}
+	
 	@Override
 	public LineaVenta getOne(LineaVenta p) throws SQLException {
 		// TODO Auto-generated method stub
